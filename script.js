df29605eedc142bc6eb4d09724925af515bcfe4a
Cahzello <rizkyrasya.m@gmail.com>
2024-06-26 17:40:21 +0700

    add carousel and change ballon animation directoin

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   let hbdauido = document.getElementById("audiohbd");
+  let index = 1;
+  let firstCounter = 1;
+  let musicbtn = document.getElementById("playmusic");
+  let btnsection = document.getElementById("btnsection");
   hbdauido.volume = 0.3;
 
-  let musicbtn = document.getElementById("playmusic");
+  msgCarousel(index);
   musicbtn.addEventListener("click", () => {
     musicbtn.classList.remove("btn-light");
     musicbtn.classList.add("btn-dark");
@@ -14,16 +18,57 @@ document.addEventListener("DOMContentLoaded", () => {
       musicbtn.classList.add("btn-light");
       musicbtn.classList.remove("bg-dark");
       musicbtn.classList.add("bg-light");
-      document.querySelectorAll(`.ballon`).forEach((ballon) => {
+      document.querySelectorAll(`.ballon`).forEach((ballon, k) => {
         ballon.classList.remove("animate");
-    });
+        ballon.classList.remove("animate2");
+      });
     } else {
       hbdauido.play();
-      document.querySelectorAll(`.ballon`).forEach((ballon) => {
+      document.querySelectorAll(`.ballon`).forEach((ballon, k) => {
+        if(k % 2 == 1){
+          ballon.classList.add("animate2");
+        } else {
           ballon.classList.add("animate");
+        }
       });
+      if (firstCounter == 1) {
+        nextButton();
+        firstCounter++;
+      }
     }
   });
 
-  console.log(Date());
+  function nextButton() {
+    let nextBtn = document.createElement("button");
+    nextBtn.type = "button";
+    nextBtn.innerText = "Next";
+    nextBtn.classList.add("btn");
+    nextBtn.classList.add("btn-lg");
+    nextBtn.classList.add("fw-bold");
+    nextBtn.classList.add("border-white");
+    nextBtn.classList.add("btn-light");
+    nextBtn.classList.add("mx-2");
+    nextBtn.style.animation = "popup 2s";
+    nextBtn.addEventListener("click", (param) => {
+      // console.log(param);
+      msgCarousel(index += 1);
+    });
+    btnsection.append(nextBtn);
+  }
+
+  function msgCarousel(n) {
+    let hasil = document.getElementsByClassName("slides");
+    console.log(index);
+    if (n > hasil.length) {
+      index = 1;
+    }
+    if (n < 1) {
+      index = hasil.length;
+    }
+    for (let i = 0; i < hasil.length; i++) {
+      hasil[i].style.display = 'none';
+    }
+    hasil[index - 1].style.display = 'block';
+    console.log(index);
+  }
 });
